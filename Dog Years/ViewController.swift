76935bd1e6eab2433615e01a668ab9a92b43ec8f
Dan Huang <jhyqt5@gmail.com>
2016-06-08 19:07:24 -0600

    Add IBoutlets and actions

--- Dog Years/ViewController.swift
+++ Dog Years/ViewController.swift
@@ -10,15 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var dogHumanYears: UITextField!
+    @IBOutlet weak var dogDogYears: UILabel!
+    @IBAction func converToDogYears(sender: UIButton) {
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
